f50685382cde6a8499b97882a744858cfa80dc70
BarryGHurt <barry.hurt@protonmail.com>
2020-09-17 15:50:32 -0700

    Test git

--- ocv_live.py
+++ ocv_live.py
@@ -2,7 +2,7 @@ import cv2
 
 webcamset = '/dev/video1'
 
-# TODO: fix this one
+# TODO: fix this one or maybe not 
 webcamset2 = 'v4l2src device=/dev/video1 ! video/x-raw, width=800, height=600, framerate=24/1 ! videoconvert ! appsink'
 
 picamset = 'nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1, format=NV12 ! nvvidconv flip-method=0 ! video/x-raw, width=1920, height=1200, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
